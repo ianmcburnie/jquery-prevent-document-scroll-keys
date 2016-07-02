48e4db6249cfc7af90f1a6074ab4b1cd3352fc00
Ian McBurnie <imcburnie@ebay.com>
2016-07-02 15:13:38 -0700

    .selector() no longer supported in jQuery 3

--- jquery.preventdocumentscrollkeys.js
+++ jquery.preventdocumentscrollkeys.js
@@ -1,7 +1,7 @@
 /**
 * @file Prevent main page scroll when pressing any scroll key (e.g. down arrow, spacebar) with focus on given selector
 * @author Ian McBurnie <ianmcburnie@hotmail.com>
-* @version 0.4.0
+* @version 0.4.1
 * @requires jquery
 * @requires jquery-common-keydown
 */
@@ -9,23 +9,36 @@
     /**
     * Prevent main page scroll when pressing any scroll key (e.g. down arrow, spacebar) with focus on given selector
     *
-    * @method "jQuery.focusExit"
+    * @method "jQuery.preventDocumentScrollKeys"
     * @return {Object} chainable jQuery class
     */
     $.fn.preventDocumentScrollKeys = function preventDocumentScrollKeys() {
-        $(document).commonKeyDown();
+        var $this = $(this);
+        $(this).commonKeyDown();
         $(this).preventDocumentSpaceKeyScroll();
         $(this).preventDocumentArrowKeyScroll();
     };
 
+    /**
+    * Prevent main page scroll when pressing spacebar with focus on given selector
+    *
+    * @method "jQuery.preventDocumentSpaceKeyScroll"
+    * @return {Object} chainable jQuery class
+    */
     $.fn.preventDocumentSpaceKeyScroll = function preventDocumentSpaceKeyScroll() {
-        $(document).on('spaceKeyDown', $(this).selector, function(e) {
+        $(this).on('spaceKeyDown', function(e) {
             e.preventDefault();
         });
     };
 
+    /**
+    * Prevent main page scroll when pressing up and down arrow keys with focus on given selector
+    *
+    * @method "jQuery.preventDocumentArrowKeyScroll"
+    * @return {Object} chainable jQuery class
+    */
     $.fn.preventDocumentArrowKeyScroll = function preventDocumentArrowKeyScroll() {
-        $(document).on('upArrowKeyDown downArrowKeyDown', $(this).selector, function(e) {
+        $(this).on('upArrowKeyDown downArrowKeyDown', function(e) {
             e.preventDefault();
         });
     };
